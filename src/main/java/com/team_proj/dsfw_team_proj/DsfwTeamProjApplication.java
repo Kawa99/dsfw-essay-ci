3d54e8ff191de19cb624a951839cb3dac8125195
Harry Legge <leggehm@cardiff.ac.uk>
2025-11-17 12:20:30 +0000

    Initial commit

---
+++ src/main/java/com/team_proj/dsfw_team_proj/DsfwTeamProjApplication.java
@@ -0,0 +1,13 @@
+package com.team_proj.dsfw_team_proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsfwTeamProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsfwTeamProjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/team_proj/dsfw_team_proj/DsfwTeamProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team_proj.dsfw_team_proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsfwTeamProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
