c95fb1394952530a46788fe2cbd976ff9728f3f8
Harry <hazbow10@gmail.com>
2025-12-01 17:40:37 +0000

    Backend foundation sort of nearly there

--- src/main/java/com/team_proj/dsfw_team_proj/teams/TeamMembershipRepository.java
+++ src/main/java/com/team_proj/dsfw_team_proj/teams/TeamMembershipRepository.java
@@ -1,4 +1,13 @@
 package com.team_proj.dsfw_team_proj.teams;
 
-public class TeamMembershipRepository {
+import com.team_proj.dsfw_team_proj.auth.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface TeamMembershipRepository extends JpaRepository<TeamMembershipEntity, Integer> {
+    Optional<TeamMembershipEntity> findbyUserAndTeam(UserEntity user, TeamEntity team);
+
+    boolean existsByUserAndTeam(UserEntity user, TeamEntity team);
 }
--- src/main/java/com/team_proj/dsfw_team_proj/teams/TeamService.java
+++ src/main/java/com/team_proj/dsfw_team_proj/teams/TeamService.java
@@ -1,4 +1,9 @@
 package com.team_proj.dsfw_team_proj.teams;
 
-public class TeamService {
+import com.team_proj.dsfw_team_proj.auth.UserEntity;
+
+public interface TeamService {
+    TeamEntity createTeam(String teamName, UserEntity creator);
+    void joinTeam(String joinCode, UserEntity user);
+    boolean isManager(UserEntity user, Long teamId);
 }
