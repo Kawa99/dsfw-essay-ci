3e84b5eac80981ec0d8389186fb9fc910fc18618
Harry <hazbow10@gmail.com>
2025-12-01 18:16:12 +0000

    Create team functionality added, homepage cannot redirect back to that page yet, adding list of all teams joined on that users account next

--- src/main/java/com/team_proj/dsfw_team_proj/teams/TeamService.java
+++ src/main/java/com/team_proj/dsfw_team_proj/teams/TeamService.java
@@ -3,6 +3,7 @@ package com.team_proj.dsfw_team_proj.teams;
 import com.team_proj.dsfw_team_proj.auth.UserEntity;
 
 public interface TeamService {
+
     TeamEntity createTeam(String teamName, UserEntity creator);
     void joinTeam(String joinCode, UserEntity user);
     boolean isManager(UserEntity user, Long teamId);
