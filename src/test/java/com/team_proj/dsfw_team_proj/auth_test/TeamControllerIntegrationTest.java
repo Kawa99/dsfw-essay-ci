7a0537c3471100c087f8e6c91b98b5572a86db9c
Harry <hazbow10@gmail.com>
2025-12-11 01:03:20 +0000

    Repeated previous procedure, added more tests to controller for additional security as well as Service methods

--- src/test/java/com/team_proj/dsfw_team_proj/auth_test/TeamControllerIntegrationTest.java
+++ src/test/java/com/team_proj/dsfw_team_proj/auth_test/TeamControllerIntegrationTest.java
@@ -4,6 +4,7 @@ import com.team_proj.dsfw_team_proj.auth.UserEntity;
 import com.team_proj.dsfw_team_proj.auth.UserService;
 import com.team_proj.dsfw_team_proj.teams.TeamEntity;
 import com.team_proj.dsfw_team_proj.teams.TeamRepository;
+import com.team_proj.dsfw_team_proj.teams.TeamService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -16,7 +17,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -32,6 +33,9 @@ public class TeamControllerIntegrationTest {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private TeamService teamService;
 
     @Test
     @WithMockUser(username = "manager@gov.uk")
@@ -64,5 +68,68 @@ public class TeamControllerIntegrationTest {
         assertNotNull(savedTeam.getJoinCode());
         assertEquals(6, savedTeam.getJoinCode().length());
         assertNotEquals("SecurePass1!", savedTeam.getPassword());
+    }
+
+    @Test
+    @WithMockUser(username = "user@gov.uk")
+    public void shouldFailCreationWhenPasswordsDoNotMatch() throws Exception {
+        UserEntity user = new UserEntity();
+        user.setEmail("user@gov.uk");
+        user.setFirstName("Test");
+        user.setLastName("User");
+        user.setPassword("pass");
+        userService.save(user);
+
+        mockMvc.perform(post("/teams/create")
+                        .param("teamName", "Mis-match Team")
+                        .param("description", "Desc")
+                        .param("password", "Password123!")
+                        .param("passwordConfirm", "Different123!")
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(view().name("teams/create-team"))
+                .andExpect(model().attributeExists("error"))
+                .andExpect(model().attribute("error", "Passwords do not match"));
+    }
+
+    @Test
+    @WithMockUser(username = "user@gov.uk")
+    public void shouldReturnForbiddenWhenNonManagerUpdatesTeam() throws Exception {
+        // 1. Create a Manager and a Team
+        UserEntity manager = new UserEntity();
+        manager.setEmail("manager@gov.uk");
+        manager.setFirstName("M"); manager.setLastName("M"); manager.setPassword("p");
+        userService.save(manager);
+        TeamEntity team = teamService.createTeam("Target Team", "Desc", "Pass123!", manager);
+
+        // 2. Create a normal User who tries to hack/update the team
+        UserEntity hacker = new UserEntity();
+        hacker.setEmail("user@gov.uk");
+        hacker.setFirstName("H"); hacker.setLastName("H"); hacker.setPassword("p");
+        userService.save(hacker);
+
+        // 3. Attempt update as 'user@gov.uk' (the hacker)
+        mockMvc.perform(put("/teams/" + team.getId() + "/name")
+                        .param("newName", "Hacked Name")
+                        .with(csrf()))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithMockUser(username = "user@gov.uk")
+    public void shouldReturnBadRequestIfConfirmPasswordMismatch() throws Exception {
+        // Setup user and team
+        UserEntity user = new UserEntity();
+        user.setEmail("user@gov.uk");
+        user.setFirstName("U"); user.setLastName("K"); user.setPassword("p");
+        userService.save(user);
+        TeamEntity team = teamService.createTeam("My Team", "Desc", "Pass123!", user);
+
+        mockMvc.perform(put("/teams/" + team.getId() + "/password")
+                        .param("currentPassword", "Pass123!")
+                        .param("newPassword", "NewPass1!")
+                        .param("confirmPassword", "Mismatch!")
+                        .with(csrf()))
+                .andExpect(status().isBadRequest());
     }
 }
